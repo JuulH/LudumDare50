a9113a442579ec3074f76b839cc607cf1c6f3ce2
Dean Isseyegh <issdea@gmail.com>
2022-04-04 17:45:44 +0100

    Fix enemy a.i for house attackers

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
 
@@ -10,23 +9,19 @@ public class EnemyAI : MonoBehaviour
     private AIPath _aiPath;
     private Animator _animator;
 
-    private GameObject _currentTarget;
+    private Transform _currentTarget;
     private float _attackDistance;
 
     [SerializeField] private GameObject enemyProjectile;
     [SerializeField] private float attackCooldown = 1.5f;
     [SerializeField] private float throwAttackStartUp = 0.5f;
-    [SerializeField] private float houseAttackOffsetDistance = 1.5f;
     private float _timeSinceLastAttack = 999f;
     private bool _isAttacking;
     private bool _isHouseAttacker;
 
-    private SoundManager _soundManager;
-    
     private static readonly int Throwing = Animator.StringToHash("Throwing");
     private static readonly int IsMoving = Animator.StringToHash("IsMoving");
     private static readonly int IsStanding = Animator.StringToHash("IsStanding");
-    private Rigidbody2D _rb;
 
     void Awake()
     {
@@ -34,49 +29,59 @@ public class EnemyAI : MonoBehaviour
         _animator = GetComponent<Animator>();
         _house = GameObject.FindWithTag("House");
         _player = GameObject.FindWithTag("Player");
-        _soundManager = GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>();
-        _rb = GetComponent<Rigidbody2D>();
         int random = Random.Range(0, 2);
-        _currentTarget = (random == 0) ? _player : _house;
-        if (_currentTarget == _house)
+        _currentTarget = (random == 0) ? _player.transform : _house.transform;
+        if (_currentTarget == _house.transform)
         {
-            _attackDistance = _aiPath.endReachedDistance + houseAttackOffsetDistance;
+            _attackDistance = _aiPath.endReachedDistance;
+            _aiPath.endReachedDistance -= 3f;
             _isHouseAttacker = true;
+            Transform houseTarget = _house.transform;
+            float closestDist = 999;
+            GameObject houseDestinations = GameObject.FindGameObjectWithTag("HouseDestinations");
+            foreach (Transform x in houseDestinations.transform)
+            {
+                float distance = Vector2.Distance(x.transform.position, this.transform.position);
+                if (distance < closestDist)
+                {
+                    houseTarget = x;
+                    closestDist = distance;
+                }
+            }
+
+            _currentTarget = houseTarget;
         }
         else
         {
             _attackDistance = _aiPath.endReachedDistance;
         }
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         _timeSinceLastAttack += Time.deltaTime;
-        if (_isAttacking)
+        _aiPath.destination = _currentTarget.position;
+
+        float distanceToTarget;
+        if (_isHouseAttacker)
         {
-            _aiPath.canMove = false;
+            distanceToTarget = Vector2.Distance(_house.transform.position, transform.position);
         }
         else
         {
-            _aiPath.canMove = true;
+            distanceToTarget = Vector2.Distance(_currentTarget.position, transform.position);
         }
-        _aiPath.destination = _currentTarget.transform.position;
 
-        float distanceToTarget = Vector2.Distance(_currentTarget.transform.position, transform.position);
-        if (distanceToTarget <= _attackDistance)
+        if (distanceToTarget <= _attackDistance || _aiPath.reachedEndOfPath)
         {
             if (!_isAttacking && _timeSinceLastAttack > attackCooldown)
             {
-                StartCoroutine(DoAttack());
+                Transform target = _isHouseAttacker ? _house.transform : _currentTarget;
+                StartCoroutine(DoAttack(target));
             }
+
             _animator.SetBool(IsMoving, false);
             _animator.SetBool(IsStanding, true);
-            if (_isHouseAttacker) {
-                _aiPath.enabled = false; //So house attackers stop once they attack house, and just stand there. As pathfinding for house is weird and buggy
-                _rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            }
         }
         else
         {
@@ -85,18 +90,20 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private IEnumerator DoAttack()
+    private IEnumerator DoAttack(Transform target)
     {
         _isAttacking = true;
+        _aiPath.canMove = false;
         _animator.SetTrigger(Throwing);
         yield return new WaitForSeconds(throwAttackStartUp);
         Vector2 enemyPosition = transform.position;
-        Vector2 targetPosition = _currentTarget.transform.position;
-        Vector2 dirToTarget = targetPosition - enemyPosition ;
-        float angle = Mathf.Atan2(dirToTarget.y, dirToTarget.x)  * Mathf.Rad2Deg - 90f;
+        Vector2 targetPosition = target.position;
+        Vector2 dirToTarget = targetPosition - enemyPosition;
+        float angle = Mathf.Atan2(dirToTarget.y, dirToTarget.x) * Mathf.Rad2Deg - 90f;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         Instantiate(enemyProjectile, transform.position, rotation);
         _isAttacking = false;
+        _aiPath.canMove = true;
         _timeSinceLastAttack = 0;
     }
 }
